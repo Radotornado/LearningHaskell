4ec07154b6c49c5e65125ed898662a5ef77915ff
Radoslav Mandev <radoslav.mandev99@gmail.com>
2019-11-23 14:12:47 +0100

    Delete Aufgabe3.hs

--- TutoriumWS18,19/1/Aufgabe3.hs
+++
@@ -1,8 +0,0 @@
-my_day :: Int -> String
-my_day x | x == 1 = "Montag"
-         | x == 2 = "Dienstag"
-         | x == 3 = "Mittwoch"
-         | x == 4 = "Donnerstag"
-         | x == 5 = "Freitag"
-         | x == 6 = "Samstag"
-         | x == 7 = "Sontag"
